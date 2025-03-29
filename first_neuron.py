512141b37978ffbe819d6a517b764f456f2feb79
Jorge Chavira <jorgechavira4@gmail.com>
2025-03-29 11:52:41 -0600

    Utilizacion de numpy

--- first_neuron.py
+++ first_neuron.py
@@ -1,6 +1,8 @@
+
 # A single neuron
 """
     Los inputs son la informacion la cual se entrega a las neuronas para su evaluacion
+"""
 """
 inputs = [1,2,3, 2.5] # Building block of a neural network
 
@@ -14,5 +16,19 @@ output  = (inputs[0]*weights[0]+
 
 print(output)
 """
+
+"""
     La neurona utiliza los inputs para calcular el output, el resultado depende del peso de ese valor y el bias que se le tiene al mismo
 """
+
+# Asi mismo se es posible utilzar la libreria de numpy para simplificar la sintaxis de las operaciones
+import numpy as np
+
+inputs = [1,2,3, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2 
+
+output = np.dot(inputs, weights) + bias
+# output = np.dot([1,2,3, 2.5], [0.2, 0.8, -0.5, 1.0]) + bias
+
+print(output)
